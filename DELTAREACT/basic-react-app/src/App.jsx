332c67a3df35821382249d553c08a3fbe4f83065
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-22 17:54:19 +0530

    Import-Export

--- DELTAREACT/basic-react-app/src/App.jsx
+++ DELTAREACT/basic-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css"
 
+import Title  from "./Title.jsx";
 
 
 function Description(){
--- DELTAREACT/basic-react-app/src/Title.jsx
+++ DELTAREACT/basic-react-app/src/Title.jsx
@@ -1,3 +1,6 @@
 function Title() {
   return <h1>This is Title</h1>;
 }
+
+export default Title;
+// export {Title};
