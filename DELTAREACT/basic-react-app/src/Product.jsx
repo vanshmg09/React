af25305fdb62d62943264c9f4c78e764133558eb
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-23 16:20:02 +0530

    Passing Array to Props

--- DELTAREACT/basic-react-app/src/Product.jsx
+++ DELTAREACT/basic-react-app/src/Product.jsx
@@ -1,11 +1,14 @@
 import "./Product.css"
 
 
-function Product({title,price = 1}) {
+function Product({title,price, features , features2}) {
+    console.log(features2.b)
     return(
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price : {price} </h5>
+            <p>{features}</p>
+            <p>{features2.b}</p>
         </div>
     )
 }
--- DELTAREACT/basic-react-app/src/ProductTab.jsx
+++ DELTAREACT/basic-react-app/src/ProductTab.jsx
@@ -1,13 +1,15 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
+  let options = ["hi-Tech" ,"durable", "fast"];
+  let options2 = {a: "hi-Tech" ,b: "durable", c: "fast"};
     return(
     <>
-      <Product title="phone" price= {30000}/>
-      <Product title="laptop" price= {4000}/>
-      <Product title="tab" />
+      <Product title="phone" price= {30000} features={options} features2={options2} />
+      <Product title="laptop" price= {4000}  features={options} features2={options2}/>
+      <Product title="tab" price={300}   features={options} features2={options2}/>
     </>
-    )
+    );
 };
 
 export default ProductTab;
