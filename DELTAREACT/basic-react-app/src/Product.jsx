ea58a799e09c10c56da47ff82db77773eeb0fe30
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-23 16:37:31 +0530

    Rendering Arrays

--- DELTAREACT/basic-react-app/src/Product.jsx
+++ DELTAREACT/basic-react-app/src/Product.jsx
@@ -1,14 +1,13 @@
 import "./Product.css"
 
 
-function Product({title,price, features , features2}) {
-    console.log(features2.b)
+function Product({title,price, features }) {
+    // let list = features.map((feature) => <li>{feature}</li>)
     return(
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price : {price} </h5>
-            <p>{features}</p>
-            <p>{features2.b}</p>
+            <p> {features.map((feature) => <li>{feature}</li>)}</p>
         </div>
     )
 }
