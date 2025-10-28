ac6654b4d82207bf784acc525ec528a84097153a
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-28 17:27:44 +0530

    Callback in Set State Function

--- DELTAREACT/react-state-class/src/App.jsx
+++ DELTAREACT/react-state-class/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <p> State in React</p>
-      <LikeButton/>
+      <Counter/>
     </>
   )
 }
--- DELTAREACT/react-state-class/src/Counter.jsx
+++ DELTAREACT/react-state-class/src/Counter.jsx
@@ -2,10 +2,23 @@ import { useState } from "react"
 
 export default function Counter(){
 // let [stateVariable, setStateVariable] = useState(10);
-let [count , setCount] = useState(0);
+let [count , setCount] = useState(0); // Initialization
 
 let incCount = () => {
-    setCount(count + 1);
+    // setCount((count) => {
+    //     return count += 1;
+    // } );
+
+    // setCount((count) => {
+    //     return count += 1;
+    // } );
+
+    // setCount((count) => {
+    //     return count += 1;
+    // } );
+
+    setCount(25);
+
 }
 
     return(
