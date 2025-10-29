86cc6b354209a8b0818b18521e82cd82b777a329
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-29 23:28:44 +0530

    Lottery Component (Lifting State Up)

--- REACT/react-state-class2/src/App.jsx
+++ REACT/react-state-class2/src/App.jsx
@@ -2,18 +2,14 @@ import './App.css'
 import LudoBoard from './LudoBoard'
 import TodoList from './TodoList'
 import Lottery from './Lottery'
-import TicketNum from './TicketNum'
-import Ticket from './Ticket'
+
 
 function App() {
 
 
   return (
     <>
-      <Ticket ticket={[1,2,3]}/>
-      <Ticket ticket={[1,2,3,6,8,5,4,7]}/>
-
-
+      <Lottery n={3} winningSum={15}/>
     </>
   )
 }
--- REACT/react-state-class2/src/Lottery.jsx
+++ REACT/react-state-class2/src/Lottery.jsx
@@ -1,25 +1,21 @@
 import { useState } from "react";
-import "./Lottery.css"
 import { genTicket, sum } from "./helper";
+import Ticket from "./Ticket";
 
-export default function Lottery(){
+export default function Lottery({ n=3, winningSum=15 }){
 
-    let [ticket, setTicket] = useState(genTicket(3));
-    let isWinning = sum(ticket) === 15 ;
+    let [ticket, setTicket] = useState(genTicket(n));
+    let isWinning = sum(ticket) === winningSum ;
 
     let buyTicket = () => {
-        setTicket(genTicket(3));
+        setTicket(genTicket(n));
     }
 
     return(
         <div>
             <h1>Lottery Game!</h1>
-            <div className="ticket">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
-            <br /><br />
+            <Ticket ticket={ticket}/>
+
             <button onClick={buyTicket}>Buy New Ticket</button>
             <h3>{isWinning && "Congratulations, you won!"}</h3>
         </div>
