389c83f4fe6b95aa8248d6799cb2e7d312a35631
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-29 23:54:34 +0530

    Functions as Props

--- REACT/react-state-class2/src/App.jsx
+++ REACT/react-state-class2/src/App.jsx
@@ -2,14 +2,18 @@ import './App.css'
 import LudoBoard from './LudoBoard'
 import TodoList from './TodoList'
 import Lottery from './Lottery'
+import { sum } from './helper'
 
 
 function App() {
 
+  let winCondition = (ticket) => {
+    return ticket[0] === 0 ;
+  }
 
   return (
     <>
-      <Lottery n={3} winningSum={15}/>
+      <Lottery n={3} winCondition={winCondition}/>
     </>
   )
 }
--- REACT/react-state-class2/src/Lottery.jsx
+++ REACT/react-state-class2/src/Lottery.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { genTicket, sum } from "./helper";
 import Ticket from "./Ticket";
+import Button from "./Button";
 
-export default function Lottery({ n=3, winningSum=15 }){
+export default function Lottery({ n=3, winCondition }){
 
     let [ticket, setTicket] = useState(genTicket(n));
-    let isWinning = sum(ticket) === winningSum ;
+    let isWinning = winCondition(ticket) ;
 
     let buyTicket = () => {
         setTicket(genTicket(n));
@@ -16,7 +17,7 @@ export default function Lottery({ n=3, winningSum=15 }){
             <h1>Lottery Game!</h1>
             <Ticket ticket={ticket}/>
 
-            <button onClick={buyTicket}>Buy New Ticket</button>
+            <Button action={buyTicket}/>
             <h3>{isWinning && "Congratulations, you won!"}</h3>
         </div>
     )
