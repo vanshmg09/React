59bf212050702897a984522b64ca15afb41e8a44
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-29 21:30:55 +0530

    Lottery Ticket (Part a)

--- REACT/react-state-class2/src/App.jsx
+++ REACT/react-state-class2/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import LudoBoard from './LudoBoard'
 import TodoList from './TodoList'
+import Lottery from './Lottery'
 
 function App() {
 
 
   return (
     <>
-      <TodoList/>
+      <Lottery/>
     </>
   )
 }
