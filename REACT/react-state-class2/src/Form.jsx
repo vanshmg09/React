753c909c7c6e69bc31241d737b2cb6c22fd09039
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-10-30 11:24:25 +0530

    Lables in React

--- REACT/react-state-class2/src/Form.jsx
+++ REACT/react-state-class2/src/Form.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 
 export default function Form(){
 
-    let [fullName, setFullName] = useState("vansh");
+    let [fullName, setFullName] = useState("");
 
     let handleNameChange = (event) => {
         setFullName(event.target.value)
     }
     return(
         <form>
-            <input placeholder="enter your full name" type="text" value={fullName} onChange={handleNameChange}/>
+            <label htmlFor="fullName">Full Name</label>
+            <input placeholder="enter your full name" type="text" value={fullName} onChange={handleNameChange} id="fullName"/>
             <button>Submit</button>
         </form>
     )
